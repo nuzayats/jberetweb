dad603ac67e821aa9f9602fdb7c025c0d16a03e3
lbtc-xxx <lbtc-xxx@bridge9.sakura.ne.jp>
2015-01-04 17:50:42 +0900

    add instance id to the table

--- src/main/java/org/nailedtothex/jberetweb/dto/JobExecutionTableRowDto.java
+++ src/main/java/org/nailedtothex/jberetweb/dto/JobExecutionTableRowDto.java
@@ -5,14 +5,16 @@ import java.util.Date;
 
 public class JobExecutionTableRowDto implements Serializable {
     Integer jobExecutionId;
+    Long jobInstanceId;
     String jobName;
     Date startTime;
     Date endTime;
     String batchStatus;
     JobExecutionAction action = JobExecutionAction.SELECT;
 
-    public JobExecutionTableRowDto(Integer jobExecutionId, String jobName, Date startTime, Date endTime, String batchStatus) {
+    public JobExecutionTableRowDto(Integer jobExecutionId, Long jobInstanceId, String jobName, Date startTime, Date endTime, String batchStatus) {
         this.jobExecutionId = jobExecutionId;
+        this.jobInstanceId = jobInstanceId;
         this.jobName = jobName;
         this.startTime = startTime;
         this.endTime = endTime;
@@ -70,10 +72,19 @@ public class JobExecutionTableRowDto implements Serializable {
         this.batchStatus = batchStatus;
     }
 
+    public Long getJobInstanceId() {
+        return jobInstanceId;
+    }
+
+    public void setJobInstanceId(Long jobInstanceId) {
+        this.jobInstanceId = jobInstanceId;
+    }
+
     @Override
     public String toString() {
         return "JobExecutionTableRowDto{" +
                 "jobExecutionId=" + jobExecutionId +
+                ", jobInstanceId=" + jobInstanceId +
                 ", jobName='" + jobName + '\'' +
                 ", startTime=" + startTime +
                 ", endTime=" + endTime +
